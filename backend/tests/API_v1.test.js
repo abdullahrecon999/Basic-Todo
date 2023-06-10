1b84a9cbbda6623534df09c01ce2f79e39be6e39
abdullahrecon999 <abdullahrecon999@gmail.com>
2023-06-10 23:26:36 +0500

    refact(API_v1.test.js): Refactored test cases with seperate test+expected data

--- backend/tests/API_v1.test.js
+++ backend/tests/API_v1.test.js
@@ -1,16 +1,32 @@
-const request = require('supertest')
-const app = require('../app')
-const Task = require('../model/task')
-const mongoose = require('mongoose')
-const testDB = process.env.MONGOURI_TEST
-const BASE_URL = '/api/v1'
+const request = require('supertest');
+const app = require('../app');
+const Task = require('../model/task');
+const mongoose = require('mongoose');
+const testDB = process.env.MONGOURI_TEST;
+const BASE_URL = '/api/v1';
+
+// Define test dataset
+const taskDataset = {
+  title: 'Task 1',
+};
+
+// Define expected values
+const expectedValues = {
+  createdTaskTitle: 'Task 1',
+  errorTaskCreation: 'Failed to create task',
+  errorTaskDeletion: 'Failed to delete task',
+  tasksNotFound: 'Tasks not found',
+  taskDeletedMessage: 'Task deleted',
+  activeStatus: 'active',
+  checkedStatus: 'checked',
+};
 
 beforeAll(async () => {
   await mongoose.connect(testDB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   });
 });
 
@@ -18,11 +34,13 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
+// Test server status
 describe('Server status test', () => {
   it('should return simple 200', async () => {
     const res = await request(app)
       .get(BASE_URL)
       .expect(200);
+
     expect(res.body).toEqual({ data: 'Api Router called' });
   });
 
@@ -30,14 +48,19 @@ describe('Server status test', () => {
     const res = await request(app)
       .get(BASE_URL+'/abc')
       .expect(404);
+
     expect(res.body).toEqual({ Error: `Cant Find ${BASE_URL}/abc` });
   });
 });
 
-describe('Get /tasks', () => {
-  // jest.setTimeout(300000);
+// Test GET /tasks endpoint
+describe('GET /tasks', () => {
+  afterEach(async () => {
+    await Task.deleteMany();
+  });
+
   it('should return all tasks with status 200', async () => {
-    const task = new Task({ title: 'Task 1' });
+    const task = new Task(taskDataset);
     await task.save();
 
     const res = await request(app)
@@ -54,56 +77,54 @@ describe('Get /tasks', () => {
       .get(BASE_URL+'/tasks')
       .expect(404);
 
-    expect(res.body).toEqual({ error: 'Tasks not found' });
+    expect(res.body).toEqual({ error: expectedValues.tasksNotFound });
   });
 });
 
+// Test POST /tasks endpoint
 describe('POST /tasks', () => {
   afterEach(async () => {
     await Task.deleteMany();
   });
 
   it('should create a new task and return status 200', async () => {
-    const taskData = { title: 'Task 1' };
-
     const res = await request(app)
       .post(BASE_URL+'/tasks')
-      .send(taskData)
+      .send(taskDataset)
       .expect(200);
 
     expect(res.body).toHaveProperty('_id');
-    expect(res.body.title).toBe('Task 1');
+    expect(res.body.title).toBe(expectedValues.createdTaskTitle);
 
     const createdTask = await Task.findById(res.body._id);
     expect(createdTask).toBeDefined();
-    expect(createdTask.title).toBe('Task 1');
+    expect(createdTask.title).toBe(expectedValues.createdTaskTitle);
   });
 
   it('should return status 500 and error message if task creation fails', async () => {
-    const taskData = {};
-
     const res = await request(app)
       .post(BASE_URL+'/tasks')
-      .send(taskData)
+      .send({})
       .expect(500);
 
-    expect(res.body).toEqual({ error: 'Failed to create task' });
+    expect(res.body).toEqual({ error: expectedValues.errorTaskCreation });
   });
 });
 
+// Test DELETE /tasks/:id endpoint
 describe(`DELETE ${BASE_URL}/tasks/:id`, () => {
   afterEach(async () => {
     await Task.deleteMany();
   });
 
   it('should delete the specified task and return status 200', async () => {
-    const task = await Task.create({ title: 'Task 1' });
+    const task = await Task.create(taskDataset);
 
     const res = await request(app)
       .delete(BASE_URL+`/tasks/${task._id}`)
       .expect(200);
 
-    expect(res.body).toEqual({ message: 'Task deleted' });
+    expect(res.body).toEqual({ message: expectedValues.taskDeletedMessage });
 
     const deletedTask = await Task.findById(task._id);
     expect(deletedTask).toBeNull();
@@ -111,55 +132,56 @@ describe(`DELETE ${BASE_URL}/tasks/:id`, () => {
 
   it('should return status 500', async () => {
     const invalidTaskId = 'invalid_id';
-
     const res = await request(app)
       .delete(BASE_URL+`/tasks/${invalidTaskId}`)
       .expect(500);
 
-    expect(res.body).toEqual({ error: 'Failed to delete task' });
+    expect(res.body).toEqual({ error: expectedValues.errorTaskDeletion });
   });
 });
 
+// Test PATCH /tasks/:id/check endpoint
 describe(`PATCH ${BASE_URL}/tasks/:id/check`, () => {
   afterEach(async () => {
     await Task.deleteMany();
   });
 
   it('should update the specified task status', async () => {
-    const task = await Task.create({ title: 'Task 1', status: 'active' });
+    const task = await Task.create({ ...taskDataset, status: expectedValues.activeStatus });
 
     const res = await request(app)
       .patch(BASE_URL+`/tasks/${task._id}/check`)
       .expect(200);
 
-    expect(res.body.title).toBe('Task 1');
-    expect(res.body.status).toBe('checked');
+    expect(res.body.title).toBe(taskDataset.title);
+    expect(res.body.status).toBe(expectedValues.checkedStatus);
     expect(res.body.checkedAt).toBeDefined();
 
     const updatedTask = await Task.findById(task._id);
-    expect(updatedTask.status).toBe('checked');
+    expect(updatedTask.status).toBe(expectedValues.checkedStatus);
     expect(updatedTask.checkedAt).toBeDefined();
   });
 });
 
+// Test PATCH /tasks/:id/uncheck endpoint
 describe(`PATCH ${BASE_URL}/tasks/:id/uncheck`, () => {
   afterEach(async () => {
     await Task.deleteMany();
   });
 
   it('should update the specified task status', async () => {
-    const task = await Task.create({ title: 'Task 1', status: 'checked' });
+    const task = await Task.create({ ...taskDataset, status: expectedValues.checkedStatus });
 
     const res = await request(app)
       .patch(BASE_URL+`/tasks/${task._id}/uncheck`)
       .expect(200);
 
-    expect(res.body.title).toBe('Task 1');
-    expect(res.body.status).toBe('active');
+    expect(res.body.title).toBe(taskDataset.title);
+    expect(res.body.status).toBe(expectedValues.activeStatus);
     expect(res.body.checkedAt).toBeDefined();
 
     const updatedTask = await Task.findById(task._id);
-    expect(updatedTask.status).toBe('active');
+    expect(updatedTask.status).toBe(expectedValues.activeStatus);
     expect(updatedTask.checkedAt).toBeDefined();
   });
 });
